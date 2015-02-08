4ae9a1aff6d1a8ccfa40e0b21f81d16bfcd4eeb9
Václav Blažej <blazeva1@fit.cvut.cz>
2015-02-08 19:51:47 +0100

    basic structure

---
+++ model/src/main/java/com/mycompany/model/impl/FamilyRelation.java
@@ -0,0 +1,11 @@
+package com.mycompany.model.impl;
+
+import com.mycompany.model.AbstractRelation;
+
+/**
+ *
+ * @author vaclavblazej
+ */
+public class FamilyRelation extends AbstractRelation{
+    
+}
--- model/src/main/java/com/mycompany/model/impl/PersonImpl.java
+++ model/src/main/java/com/mycompany/model/impl/PersonImpl.java
@@ -1,15 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.mycompany.model.impl;
+
+import com.mycompany.model.Person;
+import java.util.Date;
 
 /**
  *
  * @author Pajcak
  */
-public class PersonImpl {
-    
+public class PersonImpl implements Person{
+    private Date birth;
+    private Date death;
+    private FamilyRelation familyRelation;
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Date getDeath() {
+        return death;
+    }
+
+    public void setDeath(Date death) {
+        this.death = death;
+    }
+
+    public FamilyRelation getFamilyRelation() {
+        return familyRelation;
+    }
+
+    public void setFamilyRelation(FamilyRelation familyRelation) {
+        this.familyRelation = familyRelation;
+    }
 }
