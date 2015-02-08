a340c137c69158015ebdba1c28e34d1d8fb4e853
pajcak <Patrik.Faistaver@centrum.cz>
2015-02-08 18:56:04 +0100

    init

---
+++ model/src/main/java/com/mycompany/model/AbstractRelation.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Pajcak
+ */
+public abstract class AbstractRelation {
+    private Date start;
+    private Date end;
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+}
